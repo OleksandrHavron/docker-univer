c2ca69a90275eaad11ae561587f03d3419d03f05
OleksandrHavron <oleksandr.havron@gmail.com>
2023-05-07 18:49:45 +0000

    service1 done

--- Lab8/service1/field_service.py
+++ Lab8/service1/field_service.py
@@ -1,13 +1,12 @@
-# from kafka import KafkaProducer
-import asyncio
 import json
 import os
 from time import sleep
 
 import psycopg2
+from kafka import KafkaProducer
 
 # З'єднання з Kafka
-# producer = KafkaProducer(bootstrap_servers='kafka:9092')
+producer = KafkaProducer(bootstrap_servers='kafka:9092')
 POSTGIS_HOST = os.environ.get("POSTGRES_HOST")
 POSTGIS_PORT = os.environ.get("POSTGRES_PORT")
 POSTGIS_USER = os.environ.get("POSTGRES_USER")
@@ -32,11 +31,8 @@ def insert_field_to_db(field):
     cur = conn.cursor()
     try:
         cur.execute("INSERT INTO features (id, name, geometry) VALUES (%s, %s, ST_SetSRID(ST_GeomFromGeoJSON(%s), 4326))", (field['id'], field['name'], json.dumps(field['geom'])))
-        #cur.execute("INSERT INTO fields (id, geom) VALUES (%s, ST_SetSRID(ST_GeomFromGeoJSON(%s), 4326))", (, ))
         conn.commit()
         print(f"Field with ID {field['id']} inserted into the database.")
-        # producer.send('field-reading', json.dumps(field).encode('utf-8'))
-        print(f"Field data sent to Kafka.")
     except Exception as e:
         print(f"Error inserting field into the database: {str(e)}")
     finally:
@@ -56,26 +52,23 @@ def read_fields_from_file():
                 'geom': feature['geometry']}
             insert_field_to_db(field)
         os.remove(filename)
+        producer.send('field-reading', "Data received".encode("UTF-8"))
+        print(f"Field data sent to Kafka.")
     except Exception as e:
         print(f"Error reading file: {str(e)}")
 
 # Check and run read_fiealds_from_file() if file exists
-async def check_new_file():
+def check_new_file():
     while True:
         if os.path.exists(filename):
             read_fields_from_file()
             print("Data saved into db.")
-        else:
-            print("No file found.")
         sleep(5)
+
 
 if __name__ == "__main__":
     print("Service started")
     try:
-        loop = asyncio.get_event_loop()
-        loop.run_forever()
-        loop.create_task(check_new_file())
+        check_new_file()
     except KeyboardInterrupt:
         print("Service stopped")
-        loop.close()
-        exit(1)
